d4ed4e81fae88a700794ca7822967bfcbade8665
sevegnps-eng <sevegnps@gmail.com>
2026-03-05 22:29:57 -0300

    Criada a função de validar senha Nesta função foi usado diversos fatores para fazer a validação da senha

---
+++ main.py
@@ -0,0 +1,70 @@
+import bcrypt
+
+usuarios = []
+
+# Classe Usuário
+class Usuario():
+    def __init__(self, nome, cpf, senha):
+        self.nome = nome
+        self.cpf = cpf
+        self.validar_senha(senha)
+
+    def validar_senha(self, senha):
+
+        if len(senha) < 12:
+            raise ValueError("Sua senha deve ter no mínimo 12 caracteres")
+        elif not any(maiuscula.isupper() for maiuscula in senha):
+            raise ValueError("Sua senha deve conter no mínimo um caractere maiúsculo")
+        elif not any(minuscula.islower() for minuscula in senha):
+            raise ValueError("Sua senha deve conter no mínimo um caractere minúsculo")     
+        elif not any(numero.isdigit() for numero in senha):
+            raise ValueError("Sua senha deve conter no mínimo um número")    
+        elif not any(not simbolo.isalnum() for simbolo in senha):
+            raise ValueError("Sua senha deve conter símbolos")
+        
+# Classe Conta:
+class Conta():
+    def __init__(self, numero_conta, usuario):
+        self.numero_conta = numero_conta
+        self.usuario = usuario
+        self.saldo = 0
+        self.historico = []
+  
+    def depositar(self, valor):
+        if valor <= 0:
+            raise ValueError("Valor inválido para depósito")
+        self.saldo += valor
+        self.historico.append(f"Você depositou R${valor:.2f}")
+
+    def sacar(self, valor):
+        if valor > self.saldo:
+            raise ValueError("Saldo insuficiente")
+        if valor <= 0:
+            raise ValueError("Valor inválido para saque")
+        self.saldo -= valor
+        self.historico.append(f"Você sacou R${valor:.2f}")
+
+    def transferir(self, valor, outra_conta):
+        if valor <= 0:
+            raise ValueError("Valor inválido para transferência ")
+        if valor > self.saldo:
+            raise ValueError("Você não tem saldo suficiente para transferir ")
+        self.sacar(valor)
+        outra_conta.depositar(valor)
+        self.historico.append(f"Você transferiu R${valor:.2f} para {outra_conta.usuario.nome}")
+
+    def listar_historico(self):
+        for historico in self.historico:
+            print (historico)
+
+
+gabriel_usuario = Usuario("Gabriel Sevegnani", "053.680.100-28", "26a09b2003F#")
+gabriel_conta = Conta("537", gabriel_usuario)
+juliana_usuario = Usuario("Juliana Braga", "123.412.123-23", "01dcj3gfo4spS#")
+juliana_conta = Conta("123", juliana_usuario)
+juliana_conta.depositar(1000)
+juliana_conta.sacar(100)
+juliana_conta.transferir(200, gabriel_conta)
+
+
+
